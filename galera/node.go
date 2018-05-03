99c62156d9d7f57c9b1fe56a3d5fc384e831a5f6
Gokul T P <tp.gokul@gmail.com>
2018-05-03 12:01:52 +0530

    get nodes using docker api

--- galera/node.go
+++ galera/node.go
@@ -1,7 +1,10 @@
 package galera
 
 import (
+	"context"
+
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
 // Node encapsulates details of a galera node
@@ -10,4 +13,24 @@ type Node struct {
 	Name        string
 	Port        types.Port
 	Status      string
+}
+
+// GetNodes returns existing nodes on the system
+func GetNodes(cli *client.Client) ([]Node, error) {
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	nodes := []Node{}
+	for _, container := range containers {
+		nodes = append(nodes, Node{
+			ContainerID: container.ID,
+			Name:        container.Names[0],
+			Port:        container.Ports[0],
+			Status:      container.Status,
+		})
+	}
+
+	return nodes, nil
+
 }
